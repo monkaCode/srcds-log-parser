21428dc74320105ab2e2d231b9765908e387fa9e
monkaCode <f.h612@hotmail.com>
2026-01-04 02:15:54 +0100

    added support for entity_triggered for bomb plants

--- src/parsers/entity-triggered.ts
+++ src/parsers/entity-triggered.ts
@@ -14,47 +14,33 @@ export type EntityTriggeredEventPayload = {
         | 'restart_round_(3_seconds)'
         | 'begin_bomb_defuse_with_kit'
         | 'begin_bomb_defuse_without_kit'
+        | 'bomb_begin_plant'
         | 'defused_the_bomb'
         | 'planted_the_bomb'
         | 'got_the_bomb'
         | 'dropped_the_bomb'
         | 'clantag';
 
-    value: string;
+    value?: string;
+    bombsite?: string;
 };
 
 export type EntityTriggeredEvent = IBaseEvent<'entity_triggered', EntityTriggeredEventPayload>;
 
-// "Entity<93><STEAM_1:0:12345><CT>" triggered "Defused_The_Bomb"
-// World triggered "Match_Start" on "de_inferno"
-// World triggered "Round_Start"
-// World triggered "Round_End"
-// World triggered "Game_Commencing"
-// "PlayerName<93><STEAM_1:0:12345><CT>" triggered "Begin_Bomb_Defuse_With_Kit"
-// "PlayerName<93><STEAM_1:0:12345><CT>" triggered "Begin_Bomb_Defuse_Without_Kit"
-// "PlayerName<93><STEAM_1:0:12345><CT>" triggered "Defused_The_Bomb"
-// "Irwin<3><BOT><TERRORIST>" triggered "Planted_The_Bomb"
-// "Opie<10><BOT><TERRORIST>" triggered "Dropped_The_Bomb"
-// "Bob<5><BOT><TERRORIST>" triggered "Got_The_Bomb"
 export const entityTriggeredParser = defineParser<EntityTriggeredEvent>({
     type: 'entity_triggered',
 
     patterns: [
-        concatPattern`^(?<entity>${entityRe}) triggered "(?<kind>[^"]+)"(?: \\(value "(?<value>[^"]+)"\\))?$`,
-        concatPattern`^(?<entity>${entityRe}) triggered "(?<kind>[^"]+)" on "(?<value>[^"]+)"$`,
+        concatPattern`^(?<entity>${entityRe}) triggered "(?<kind>[^"]+)"(?: \\(value "(?<value>[^"]+)"\\))?(?: at bombsite (?<bombsite>[^"]+))?$`,
+        concatPattern`^(?<entity>${entityRe}) triggered "(?<kind>[^"]+)" on "(?<value>[^"]+)"(?: at bombsite (?<bombsite>[^"]+))?$`,
     ],
 
-    parse({
-        entity,
-
-        kind,
-        value,
-    }) {
+    parse({ entity, kind, value, bombsite }) {
         return {
             entity: parseEntity(entity),
-
             kind: kind.toLowerCase() as EntityTriggeredEventPayload['kind'],
             value,
+            bombsite,
         };
     },
 });
